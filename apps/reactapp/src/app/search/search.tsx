6ced678a764e8ef8adcdfdcb8397c159fa4b8484
Sebastián Guevara <rlxsebas@gmail.com>
2019-03-17 02:36:06 -0500

    feature: setup-project - Create React app & adding material web components

---
+++ apps/reactapp/src/app/search/search.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Component } from 'react';
+import Button from '@material-ui/core/Button';
+// import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+// import { Theme } from '@material-ui/core/styles/createMuiTheme';
+// import createStyles from '@material-ui/core/styles/createStyles';
+
+// const styles = (theme: any) =>
+//     createStyles({
+//         root: {
+//             textAlign: 'center',
+//             paddingTop: theme.spacing.unit * 20,
+//         },
+//     });
+
+class Search extends Component {
+
+    render() {
+        return (
+            <div>
+                <button className="mdc-button">
+                    <span className="mdc-button__label">Button</span>
+                </button>
+
+                <h1> Hola </h1>
+
+            </div>
+        )
+    }
+}
+
+export default Search;
+
+

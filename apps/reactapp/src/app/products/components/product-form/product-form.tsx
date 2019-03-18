84dd29f3ba6d1a44e5439999979d33eb202a193b
Sebastián Guevara <rlxsebas@gmail.com>
2019-03-18 18:33:31 -0500

    feature: products - Adding listener to product form

--- apps/reactapp/src/app/products/components/product-form/product-form.tsx
+++ apps/reactapp/src/app/products/components/product-form/product-form.tsx
@@ -1,24 +1,43 @@
 import * as React from 'react';
 import "./product-form.scss";
+import { Entity } from '../../+state/products.reducer';
 
 interface Props {
     opened: boolean;
-    callbackFromParent: any;
+    productInformation: Entity
+    onClosed: any;
 }
-export default class Productform extends React.Component<Props> {
+
+export default class Productform extends React.Component<Props, Entity> {
 
     constructor(props) {
         super(props);
     }
 
+    componentWillReceiveProps(a) {
+        this.setState({ ...this.state, ...a.productInformation });
+    }
+
     printLog = (e) => {
         e.preventDefault();
-        console.log('Click Detected', this.props);
-        this.props.callbackFromParent(false);
+        this.props.onClosed(false);
     }
 
     get isOpened() {
         return this.props.opened
+    }
+
+    detectChangeOnField(field, e) {
+        e.preventDefault();
+        const obj = {};
+        obj[`${field}`] = e.target.value;
+        console.warn({ ...this.state, ...obj });
+        this.setState({ ...this.state, ...obj });
+    }
+
+    onFormSubmit(e) {
+        console.warn(this.state);
+        e.preventDefault();
     }
 
     render() {
@@ -31,32 +50,30 @@ export default class Productform extends React.Component<Props> {
                         <div className="dialog-mask dialog-mask--material"></div>
                         <div className="dialog dialog--material product-form-container" onClick={this.printLog}></div>
                         <div className="dialog dialog-container--material product-form-wrapper">
-                    <div>
-                        <h1> Add a Product</h1>
-                        <p> Please fill the data requested in this dialog to add a product to our table,
+                            <div>
+                                <h1> Add a Product {this.state.name}</h1>
+                                <p> Please fill the data requested in this dialog to add a product to our table,
                 including Product name, price and Category.</p>
-                        <span>
-                            <form>
-                                <input className="text-input text-input--underbar product-form-wrapper-input" placeholder="Product Name" type="text" required />
-                                <br />
-                                <input className="text-input text-input--underbar product-form-wrapper-input" placeholder="Price" type="text" required />
-                                <br />
-                                <input className="text-input text-input--underbar product-form-wrapper-input" placeholder="Price" type="text" required />
-                                <br />
-                                <div>
-                                    <label htmlFor="category"> Category </label>
-                                    <select id="category" className="select-input select-input--material">
-                                        <option value="Electronics">Electronics</option>
-                                        <option value="Sporting Goods">Sporting Goods</option>
-                                    </select>
-                                </div>
-                            </form>
-                        </span>
-                        <button className="button button--outline">Cancel</button>
-                        <button className="button">Add</button>
+                                <span>
+                                    <form onSubmit={this.onFormSubmit}>
+                                        <input className="text-input text-input--underbar product-form-wrapper-input" placeholder="Product Name" value={this.state.name} type="text" onChange={(e) => { this.detectChangeOnField('name', e) }} required />
+                                        <br />
+                                        <input className="text-input text-input--underbar product-form-wrapper-input" placeholder="Price" type="text" value={this.state.price} onChange={(e) => { this.detectChangeOnField('price', e) }} required />
+                                        <br />
+                                        <div>
+                                            <label htmlFor="category"> Category </label>
+                                            <select id="category" className="select-input select-input--material" value={this.state.category} onChange={(e) => { this.detectChangeOnField('category', e) }}>
+                                                <option value="Electronics">Electronics</option>
+                                                <option value="Sporting Goods">Sporting Goods</option>
+                                            </select>
+                                        </div>
+                                    </form>
+                                </span>
+                                <button className="button button--outline" type="button">Cancel</button>
+                                <button className="button" type="submit">Add</button>
 
-                    </div>
-                </div>
+                            </div>
+                        </div>
                     </React.Fragment>
                 )
                 }
--- apps/reactapp/src/app/products/components/products-category/products-category.tsx
+++ apps/reactapp/src/app/products/components/products-category/products-category.tsx
@@ -11,28 +11,49 @@ export interface Props {
 
 export interface State {
     modalIsOpened: boolean;
+    productInformation: Entity;
 }
 
 export default class ProductsCategory extends React.Component<Props, State> {
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.setModalIsOpended(false);
     }
-    
-    setModalIsOpended(value: boolean){
+
+    setModalIsOpended(value: boolean) {
         console.log('Change to', value);
-        this.setState({modalIsOpened: value})
+        this.setState({ ...this.state, modalIsOpened: value })
         console.log('Changed', this.state);
 
     }
 
-    get modalIsOpened(){
+    get modalIsOpened() {
         console.log(this.props);
         return this.state.modalIsOpened;
+    }
+
+    get productInformation() {
+        return this.state.productInformation;
+    }
+
+
+    setProductInformation(productInformation: Entity) {
+        console.log({ productInformation });
+        this.setState({ ...this.state, productInformation });
+        this.openModalForm();
+    }
+
+    closeModalForm = (dataFromChild) => {
+        console.log(dataFromChild);
+        this.setModalIsOpended(false);
+    }
+
+    openModalForm = () => {
+        this.setModalIsOpended(true);
     }
 
     buildCategoryTitle(products: Entity[]) {
@@ -53,42 +74,26 @@ export default class ProductsCategory extends React.Component<Props, State> {
 
     buildProductsByCategory(category: string, products: Entity[]) {
         return products[category].map((item) => {
-            return <React.Fragment key={item._id}> 
-            <div className="products-container">
-                <div className="products-container-item__name">{item.name}</div>
-                <div className="products-container-item__price">{item.price}</div>
-
-            </div>
+            return <React.Fragment key={item._id}>
+                <div className="products-container" onClick={() => this.setProductInformation(item)}>
+                    <div className="products-container-item__name">{item.name}</div>
+                    <div className="products-container-item__price">{item.price}</div>
+                </div>
             </React.Fragment>
         })
 
-        
-    }
 
-    myCallback = (dataFromChild) => {
-        console.log(dataFromChild);
-        this.setModalIsOpended(false);
-    }
-
-    openModalForm = (e) => {
-        e.preventDefault();
-        console.log('Click Detected', this.props);
-        this.setModalIsOpended(true);
     }
 
     render() {
         const products = this.props.products;
-        const categories = Object.keys(products);
-        console.log('mis props', this.props);
-        const a = this.buildCategoryTitle(products);
-        console.log({ a })
         return (
             <React.Fragment>
                 {!_.isEmpty(products) ? (
                     <React.Fragment>
-                        {a}
-                        <button onClick={this.openModalForm}> Add Product</button>
-                        <Productform opened={this.modalIsOpened} callbackFromParent={this.myCallback}></Productform>
+                        {this.buildCategoryTitle(products)}
+                        <button onClick={() => { this.openModalForm() }}> Add Product</button>
+                        <Productform opened={this.modalIsOpened} onClosed={this.closeModalForm} productInformation={this.productInformation}></Productform>
                     </React.Fragment>
                 ) : (<Spinner></Spinner>
                     )
